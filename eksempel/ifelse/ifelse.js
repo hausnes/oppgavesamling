24fceb588d672845cfa17fb81077844f9d9700fd
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-09-03 11:22:50 +0200

    Enkelt eksempel: if-else

---
+++ eksempel/ifelse/ifelse.js
@@ -0,0 +1,19 @@
+let alder = 8;
+
+if (alder < 16) {
+    console.log("Ha deg vekk!");
+}
+else if (alder < 18) {
+    console.log("Moped");
+}
+else if (alder < 21) {
+    console.log("Moped og bil");
+}
+else if (alder >= 21) {
+    console.log("Moped, bil og buss");
+}
+else {
+    console.log("En feil oppsto.")
+}
+
+// NB: Lag et flytskjema
