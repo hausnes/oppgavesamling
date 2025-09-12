aff15df8f630d2d48fff3d406d8244b60cdc641f
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-09-12 12:24:11 +0200

    CSS frå JS, eksempel

---
+++ temp/kode.js
@@ -0,0 +1,9 @@
+let boks1 = document.createElement("button");
+boks1.id = "boks1";
+boks1.innerText = "boks1";
+boks1.style.backgroundColor = "pink";
+boks1.style.width = "300px";
+boks1.style.display = "block";
+boks1.style.textAlign = "center";
+boks1.style.margin = "0 auto";
+document.querySelector("main").appendChild(boks1);
